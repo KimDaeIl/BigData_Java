5d19a47d6cb6b77ee086d8df1e1ad8f66a933504
Daeil Kim <daelous@naver.com>
2017-07-03 20:18:35 +0900

    Util 과제

--- workspace/01.java_workspace/14_2.ccolection_member/src/work/Util.java
+++ workspace/01.java_workspace/14_2.ccolection_member/src/work/Util.java
@@ -1,5 +1,12 @@
 package work;
 
+
+import java.text.DateFormat;
+import java.text.FieldPosition;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
 /**
  * Created by KimDaeil on 2017. 6. 29..
  */
@@ -19,7 +26,7 @@ public class Util {
         return false;
     }
 
-    public static boolean isValidString(String str, int length){
+    public static boolean isValidString(String str, int length) {
         return isValidString(str, 1, length);
     }
 
@@ -27,6 +34,97 @@ public class Util {
         return isValidString(str, length, length);
     }
 
+    public static void print(Object obj) {
+        System.out.println(obj);
+    }
+
+    public static void print() {
+        System.out.println();
+    }
+
+    /*
+    1.	암호 등의 보안문자정보 출력시 2자리만 보여주고
+	남은 문자는 *(기호)로 대체 출력위한 메서드
+
+	메서드명 : convertSecureString
+	아규먼트 : String
+	반환타입 : String
+
+	참고 : String, StringBuffer, StringBuilder
+     */
+
+    public static String convertSecureString(String data) {
+
+        if (isNull(data) || data.isEmpty()) {
+            return data;
+        }
+
+        StringBuilder resultStringBuilder = new StringBuilder(data.substring(0, data.length() > 2 ? 2 : data.length()));
+        for (int i = 0; i < data.length() - 2; i++) {
+            resultStringBuilder.append("*");
+        }
+
+
+        return resultStringBuilder.toString();
+    }
+
+    /*
+    2. 암호 분실시 임시암호 등 임의의 보안문자열 반환 메서드
+	-- 임시 보안문자열 길이는 기본은 4자리
+	-- 아규먼트로 길이 입력한 길이만큼
+	-- 메서드 중복 정의
+
+	메서드명 : getSecureString
+	아규먼트 : int
+	반환타입 : String
+
+	참고 :
+	String, StringBuffer, StringBuilder,
+	Math#random(), java.util.Random, java.lang.System.currentTimeMillis()
+     */
+
+    public static String getSecureString(int lengthOfNum) {
+        if (lengthOfNum < 4) {
+            lengthOfNum = 4;
+        }
+
+        StringBuilder resultStringBuilder = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < lengthOfNum; i++) {
+            resultStringBuilder.append(random.nextInt(10));
+        }
+
+        return resultStringBuilder.toString();
+    }
+
+    /*
+     3. 현재 날짜를 기본형식의 문자열로 반환하는 메서드
+	-- 기본형식 : 년도4자리/월2자리/일2자리
+	-- 날짜의 형식을 아규먼트로 전달받아 해당형식의 문자열 반환
+	-- 중복정의
+
+    메서드명 : getCurrentDay
+	아규먼트 : String
+	반환타입 : String
+
+	참고 :
+	String, StringBuffer, StringBuilder,
+	java.util.Date, java.text.SimpleDateFormat
+     */
+
+    public static String getCurrentDay(String dateFormatString) {
+        if (dateFormatString == null) {
+            dateFormatString = "yyyy/MM/dd";
+        }
+
+        StringBuffer resultStringBuffer = new StringBuffer();
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat(dateFormatString);
+        dateFormat.format(new Date(), resultStringBuffer, new FieldPosition(DateFormat.DEFAULT));
+
+
+        return resultStringBuffer.toString();
+    }
 
 
 }
--- workspace/01.java_workspace/14_2.ccolection_member/src/work/view/MemberTest.java
+++ workspace/01.java_workspace/14_2.ccolection_member/src/work/view/MemberTest.java
@@ -1,13 +1,18 @@
 package work.view;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Random;
 
+import work.Util;
 import work.model.dto.AdminMember;
 import work.model.dto.GeneralMember;
 import work.model.dto.Member;
 import work.model.dto.SpecialMember;
 import work.model.service.MemberService_list;
 import work.model.service.MemberService_map;
+
+import static work.Util.isNull;
 
 /**
  * ��� �׽�Ʈ Ŭ����
@@ -25,72 +30,92 @@ public class MemberTest {
      */
     public static void main(String[] args) {
 //      MemberService_list ms = new MemberService_list();
-      MemberService_list ms = new MemberService_list();
-        ms.addMember(new AdminMember());
-        ms.addMember(new GeneralMember("user00001", "password001", "", "", ""));
-        ms.addMember(new AdminMember());
-        ms.addMember(new AdminMember());
-        ms.addMember(new AdminMember());
-        ms.addMember(new AdminMember());
-        ms.addMember(new AdminMember());
-        ms.addMember(new AdminMember());
-        ms.addMember(new AdminMember());
-
-//        System.out.println(ms.isFull() ? "unavailable" : "available");
-//        System.out.println(ms.getCount());
+//        MemberService_list ms = new MemberService_list();
+//        ms.addMember(new AdminMember());
+//        ms.addMember(new GeneralMember("user00001", "password001", "", "", ""));
+//        ms.addMember(new AdminMember());
+//        ms.addMember(new AdminMember());
+//        ms.addMember(new AdminMember());
+//        ms.addMember(new AdminMember());
+//        ms.addMember(new AdminMember());
+//        ms.addMember(new AdminMember());
+//        ms.addMember(new AdminMember());
+//
+////        System.out.println(ms.isFull() ? "unavailable" : "available");
+////        System.out.println(ms.getCount());
+////        System.out.println();
+//
+//        System.out.println("=========");
+//        Member user01 = ms.getMember("user00001");
+//
+//        if (user01 == null) {
+//            System.out.println("user is not existing.");
+//            System.out.println();
+//            return;
+//        }
+//
+//        System.out.println();
+//
+//        user01.setMemberName("name0101");
+//        System.out.println(user01);
+//        System.out.println();
+//
+//        if (ms.updateMember(user01)) {
+//            System.out.println(ms.getMember("user00001"));
+//            System.out.println();
+//        }
+//
+//
+//        if (ms.updateMemberPw("user00001", "password001", "newPassowrd001")) {
+//            System.out.println(user01);
+//            System.out.println();
+//        }
+//
+//        ms.printAllMembers();
+//
+//        if (ms.updateMileage("user00001", 99999999)) {
+//            System.out.println(((GeneralMember) ms.getMember("user00001")).getMileage());
+//        }
+//        System.out.println();
+//
+//
+//        System.out.println("<< 삭제 >>");
+//        if (ms.deleteMember("user00001", "newPassowrd001")) {
+//            System.out.println("user00001>> 삭제");
+//            System.out.println();
+//        }
+//
+//        ms.printAllMembers();
+//
+//        ArrayList<Member> members = ms.getAllMembers();
+//        for (Member m : members) {
+//            System.out.println(m);
+//        }
 //        System.out.println();
 
-        System.out.println("=========");
-        Member user01 = ms.getMember("user00001");
+        Util.print("1. 암호 등의 보안문자정보 출력시 2자리만 보여주고\n" +
+                "\t남은 문자는 *(기호)로 대체 출력위한 메서드");
+        Util.print(Util.convertSecureString(null));
+        Util.print(Util.convertSecureString(""));
+        Util.print(Util.convertSecureString("1"));
+        Util.print(Util.convertSecureString("12"));
+        Util.print(Util.convertSecureString("123"));
+        Util.print(Util.convertSecureString("1234"));
+        Util.print(Util.convertSecureString("12345"));
+        Util.print();
 
-        if (user01 == null) {
-            System.out.println("user is not existing.");
-            System.out.println();
-            return;
-        }
-        
-        System.out.println();
+        Util.print("2. 암호 분실시 임시암호 등 임의의 보안문자열 반환 메서드");
+        Util.print(Util.getSecureString(4));
+        Util.print();
 
-        user01.setMemberName("name0101");
-        System.out.println(user01);
-        System.out.println();
+        Util.print("3. 현재 날짜를 기본형식의 문자열로 반환하는 메서드");
+        Util.print(Util.getCurrentDay(null));
+        Util.print();
 
-        if (ms.updateMember(user01)) {
-            System.out.println(ms.getMember("user00001"));
-            System.out.println();
-        }
-
-
-        if (ms.updateMemberPw("user00001", "password001", "newPassowrd001")) {
-            System.out.println(user01);
-            System.out.println();
-        }
-
-        ms.printAllMembers();
-        
-        if (ms.updateMileage("user00001", 99999999)) {
-            System.out.println(((GeneralMember) ms.getMember("user00001")).getMileage());
-        }
-        System.out.println();
-
-
-        System.out.println("<< 삭제 >>");
-        if (ms.deleteMember("user00001", "newPassowrd001")) {
-            System.out.println("user00001>> 삭제");
-            System.out.println();
-        }
-
-        ms.printAllMembers();
-        
-        ArrayList<Member> members = ms.getAllMembers();
-        for(Member m:members){
-            System.out.println(m);
-        }
-        System.out.println();
-
-
-
-        //
 
     }
+
+
+    //
+
 }
